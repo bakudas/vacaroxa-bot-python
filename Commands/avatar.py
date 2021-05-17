3410dbf1e465cdcfab74f348e3ac32f06e752bd5
bakudas <bakudas@gmail.com>
2021-05-17 09:28:00 -0300

    add avatar command

---
+++ Commands/avatar.py
@@ -0,0 +1,15 @@
+from discord import Member
+from discord.ext import commands
+
+@commands.command(name="avatar", help="Ver o avatar")
+async def avatar(ctx, member:Member = " "):
+
+    if member != " ":
+        await ctx.send(member.avatar_url)
+    else:
+        member = ctx.author
+        await ctx.send(member.avatar_url)
+
+
+def setup(bot):
+    bot.add_command(avatar)
