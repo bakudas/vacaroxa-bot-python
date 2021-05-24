bb8aa061c13c61300c42ee31ad7c30261ed812b8
bakudas <bakudas@gmail.com>
2021-05-24 02:26:02 -0300

    add alongamento command

---
+++ Commands/alongamento.py
@@ -0,0 +1,12 @@
+from discord.ext import commands
+from discord import File
+
+
+@commands.command(name="alongamento", help="Imagem com diversos alongamentos")
+async def alongamentos(ctx):
+
+    await ctx.send(file=File("./Images/alongamento.png"))
+
+
+def setup(bot):
+    bot.add_command(alongamentos)
