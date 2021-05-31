3b6457d02ea116ddae7540f403cd3d1ac21d3b0f
bakudas <bakudas@gmail.com>
2021-05-31 13:58:25 -0300

    add gato command

--- Commands/gato.py
+++ Commands/gato.py
@@ -1,24 +1,34 @@
 import requests
 from discord.ext import commands
+from discord import File
+import random
 
 site_api = "https://api.thecatapi.com/v1/images/search?mime_types=gif"
 
 
 @commands.command(name='gato', help="mim dá foto de gatinho")
 async def gato(ctx):
-    # faz a chamada na api
-    get = requests.get(site_api)
+    # pick a random number
+    rng = random.randint(0, 100)
 
-    if get.status_code != 200:
-        return
+    # se o rgn não for 42 entra no fluxo da api de gatinhos
+    if rng != 42:
+        # faz a chamada na api
+        get = requests.get(site_api)
 
-    # serializa a resposta
-    dict_gatinho = get.json()
+        if get.status_code != 200:
+            return
 
-    # seta a apenas a url para postar
-    url = dict_gatinho[0]['url']
+        # serializa a resposta
+        dict_gatinho = get.json()
 
-    await ctx.send(url)
+        # seta a apenas a url para postar
+        url = dict_gatinho[0]['url']
+
+        await ctx.send(url)
+    # se o rgn for 42 envia da fotenha do efe
+    else:
+        await ctx.send(file=File('./Images/effe.jpg'))
 
 
 def setup(bot):
